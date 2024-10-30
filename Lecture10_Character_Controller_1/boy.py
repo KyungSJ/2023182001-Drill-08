e03209ad0f4a20787d455262b5e27aed478acfee
KyungSJ <ksjun004@gmail.com>
2024-10-30 18:05:59 +0900

    AutoRun 상태 draw 추가

--- Lecture10_Character_Controller_1/boy.py
+++ Lecture10_Character_Controller_1/boy.py
@@ -117,6 +117,10 @@ class AutoRun:
         pass
     @staticmethod
     def draw(boy):
+        boy.image.clip_draw(
+            boy.frame * 100, boy.action * 100, 100, 100,
+            boy.x, boy.y
+        )
         pass
 
 
